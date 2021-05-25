2cffea6174e81a2296465fa29612709a2a6c7c71
naman jain <naman2001jain@gmail.com>
2021-05-25 16:18:55 +0530

    findGCD euclidean method added.

--- FindGCD.cpp
+++ FindGCD.cpp
@@ -8,6 +8,19 @@ int findGCD(int a, int b){
     }
     return findGCD(b%a,a);
 }
+//euclidean algorithm
+/*
+int findGCD(int a, int b){
+    while(a!=b){
+        if(a>b){
+            a = a-b;
+        }else{
+            b = b-a;
+        }
+    }
+    return a;
+}
+*/
 
 int main()
 {
