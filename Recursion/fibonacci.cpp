727a14b73b45dcc0405e9487dc11915668450b79
naman jain <naman2001jain@gmail.com>
2021-05-29 01:08:05 +0530

    fibonacci.cpp

---
+++ Recursion/fibonacci.cpp
@@ -0,0 +1,21 @@
+//given an integer find fibonacci of that number.
+
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n)
+    {
+        if(n<=1){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<fibonacci(n);
+    
+}
